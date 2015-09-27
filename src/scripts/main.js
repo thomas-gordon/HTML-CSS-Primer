ba7727532216f04bf8268bd490573a3dde1cf79b
Tom Gordon <tom.gordon@squareweave.com.au>
2015-09-27 14:01:28 +1000

    Add mind-blown gif and the avatar image.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,17 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   pdf = require('bespoke-pdf'),
   bullets = require('bespoke-bullets'),
   backdrop = require('bespoke-backdrop'),
   scale = require('bespoke-scale'),
+  voltaire = require('bespoke-theme-voltaire'),
   progress = require('bespoke-progress');
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  voltaire(),
   keys(),
   touch(),
   pdf(),
